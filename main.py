c19b4a148f5d7ac579b640b4ac65ae66744aefad
FaceToDog <2291449865@qq.com>
2023-07-15 20:15:37 +0800

    补丁： 1、引用相应的界面，对相应界面进行修改。

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 from PyQt5.QtCore import QObject, pyqtSignal
 from PyQt5.QtWidgets import QWidget, QApplication, QTextEdit
 from PyQt5.Qt import QThread
+from 界面.lotteryTicket import Ui_twoColorPlay
 
 
 class EmitStr(QObject):
@@ -13,26 +14,26 @@ class EmitStr(QObject):
     进而发出信号
     text：新字符串，会通过信号传递出去
     '''
-    textWrit  = pyqtSignal(str)
+    textWrit = pyqtSignal(str)
+
     def write(self, text):
         self.textWrit.emit(str(text))
 
 
-class MyWidget(QWidget):    # 定义一个窗体类，继承于QWidget
+class MyWidget(QWidget):  # 定义一个窗体类，继承于QWidget
     def __init__(self):
         super().__init__()
-        self.step_up()      # 构建一个QTextEdit， 以及对窗体的一些基本设置
-        sys.stdout = EmitStr(textWrit=self.outputWrite)     # 输出结果重定向
-        sys.stderr = EmitStr(textWrit=self.outputWrite)     # 错误输出重定向
+        self.ui = Ui_twoColorPlay()
+        self.ui.setupUi(self)
+        sys.stdout = EmitStr(textWrit=self.outputWrite)  # 输出结果重定向
+        sys.stderr = EmitStr(textWrit=self.outputWrite)  # 错误输出重定向
+        self.ui.pushButton.clicked.connect(self.printText)
 
     def outputWrite(self, text):
-        self.te.append(text)       # 输出的字符追加到 QTextEdit 中
+        self.ui.textEdit.append(text)  # 输出的字符追加到 QTextEdit 中
 
-    def step_up(self):
-        self.setWindowTitle('输出重定向')
-        self.resize(500,500)
-        self.te = QTextEdit(self)
-        self.te.move(10,10)
+    def printText(self):
+        print("hello")
 
 
 class MyThread(QThread):
@@ -40,15 +41,14 @@ class MyThread(QThread):
         super().__init__()
 
     def run(self):
-        for i in range(10):
-            print(i)
+        while True:
             time.sleep(0.5)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     app.aboutToQuit.connect(app.deleteLater)
     w = MyWidget()
-
     mythread = MyThread()
     mythread.start()
     mythread.finished.connect(lambda: print('完成打印'))
